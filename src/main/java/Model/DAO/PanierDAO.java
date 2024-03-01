3382144a67a9fbade250a54dd8f77d8711d38b61
ZhixianZHANG1 <103566696+ZhixianZHANG1@users.noreply.github.com>
2024-03-01 17:01:02 +0100

    US1.1

--- src/main/java/Model/DAO/PanierDAO.java
+++ src/main/java/Model/DAO/PanierDAO.java
@@ -1,49 +1,55 @@
 package Model.DAO;
 
+import org.hibernate.HibernateException;
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
+import Model.metier.LignePanier;
 import Model.metier.Panier;
 
 public class PanierDAO {
 
 	public Panier getPanierById(int panierId) {
-	    try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
-	        session.beginTransaction();
-	        Panier panier = session.get(Panier.class, panierId);
-	        // Charger les lignes de panier associées au panier
-	        if (panier != null) {
-	            // Forcer le chargement des lignes de panier
-	            panier.getLignesPanier().size();
-	        }
-	        session.getTransaction().commit();
-	        return panier;
-	    } catch (Exception e) {
-	        e.printStackTrace();
-	        return null;
-	    }
+		try (Session session = HibernateUtil.getSessionFactory().getCurrentSession()) {
+			session.beginTransaction();
+			Panier panier = session.get(Panier.class, panierId);
+			// Charger les lignes de panier associées au panier
+			if (panier != null) {
+				// Forcer le chargement des lignes de panier
+				panier.getLignesPanier().size();
+			}
+			session.getTransaction().commit();
+			return panier;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
 
+	public void updateLignePanierQuantity(int panierId, String productId, int newQuantity) {
+		Transaction transaction = null;
+		try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+			transaction = session.beginTransaction();
 
+			// 获取购物车中的产品
+			LignePanier lignePanier = (LignePanier) session.createQuery(
+							"FROM LignePanier WHERE panier.idPanier = :panierId AND produit.idProduit = :productId")
+					.setParameter("panierId", panierId)
+					.setParameter("productId", productId)
+					.uniqueResult();
 
+			// 更新产品数量
+			if (lignePanier != null) {
+				lignePanier.setQuantite(newQuantity);
+				session.update(lignePanier);
+			}
 
-		public static void main(String[] args) {
-		    // Création de l'instance de PanierDAO
-		    PanierDAO panierDAO = new PanierDAO();
-		
-		    // Spécifiez l'identifiant du panier que vous souhaitez récupérer pour le test
-		    int panierId = 1;
-		
-		    // Appelez la méthode getPanierById pour récupérer le panier
-		    Panier panier = panierDAO.getPanierById(panierId);
-		
-		    // Vérifiez si le panier retourné n'est pas null
-		    if (panier != null) {
-		        // Affichez les détails du panier
-		        System.out.println("ID Panier: " + panier.getIdPanier());
-		        System.out.println("Date de création: " + panier.getDateCreation());
-		        // Affichez d'autres détails du panier selon vos besoins
-		    } else {
-		        System.out.println("Le panier est introuvable.");
-		    }
+			transaction.commit();
+		} catch (HibernateException ex) {
+			if (transaction != null) {
+				transaction.rollback();
+			}
+			ex.printStackTrace();
 		}
-		}
+	}
+}
